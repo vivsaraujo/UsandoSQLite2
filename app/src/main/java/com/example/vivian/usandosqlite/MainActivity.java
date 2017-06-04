d2693a5aba57ccbd68f0c535f275ff67e0ae62ea
Vivian Araujo <vivian.araujo1994@hotmail.com>
2017-06-04 00:32:25 -0300

    Melhorias

--- app/src/main/java/com/example/vivian/usandosqlite/MainActivity.java
+++ app/src/main/java/com/example/vivian/usandosqlite/MainActivity.java
@@ -1,20 +1,14 @@
 package com.example.vivian.usandosqlite;
 
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
